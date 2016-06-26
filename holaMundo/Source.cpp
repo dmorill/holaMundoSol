729ff6c0a755e8fae703858651d04dade98b575a
Daniel Morillo Torres <dmorill@gmail.com>
2016-06-26 23:45:10 +0200

    vector Commit

--- holaMundo/Source.cpp
+++ holaMundo/Source.cpp
@@ -1,11 +1,26 @@
 #include <iostream>;
-
+#include <vector>;
 using namespace std;
 
-int main() {
+void mostrarVector(vector<int> &v);
 
+int main() {
+	vector<int> v;
+
+	for (int i = 0; i < 10; i++) {
+		v.push_back(i);
+	}
 	cout << "Hola mundo.";
+
+	mostrarVector(v);
 
 	cin.get();
 	return 0;
+}
+
+void mostrarVector(vector<int> &v) {
+	vector<int>::iterator it = v.begin();
+	for (; it != v.end(); it++) {
+		cout << *it << endl;
+	}
 }
