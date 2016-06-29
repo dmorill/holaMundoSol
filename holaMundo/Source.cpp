9f3de751344c814ee2eab3f6180604576b76577d
Daniel Morillo Torres <dmorill@gmail.com>
2016-06-29 17:16:38 +0200

    Eliminar caracteres de txt
    daniel

--- holaMundo/Source.cpp
+++ holaMundo/Source.cpp
@@ -1,26 +1,40 @@
 #include <iostream>;
-#include <vector>;
+#include <fstream>;
+#include <string>;
+#include <experimental/filesystem>;
 using namespace std;
 
-void mostrarVector(vector<int> &v);
+void path(string &s);
 
 int main() {
-	vector<int> v;
+	string donde;
+	path(donde);
+	donde = donde + "texto.txt";
+	fstream file;
 
-	for (int i = 0; i < 10; i++) {
-		v.push_back(i);
+	file.open(donde, fstream::ate | fstream::in | fstream::out);	//ate es para empezar el cursor al fnial, es diferente de ::app
+	if (file.is_open()== false) {
+		cout << "Horror..."; 
+		return 0;
 	}
-	cout << "Hola mundo.";
+	long pos = file.tellp();
+	experimental::filesystem::resize_file(donde, pos-4);
+	file.close();
 
-	mostrarVector(v);
-
-	cin.get();
+	//cin.get();
 	return 0;
 }
 
-void mostrarVector(vector<int> &v) {
-	vector<int>::iterator it = v.begin();
-	for (; it != v.end(); it++) {
-		cout << "[" <<*it <<"]" << endl;
+void path(string &s) {
+	char* pchar;
+	_get_pgmptr(&pchar);
+	string aux(pchar); int y = 0, i = 0;
+	for (i = aux.size(); i >= 0; i--) {
+		if (aux[i - 1] == char(92)) {
+			y++;
+			if (y == 1) break;
+		}
 	}
+	aux = aux.substr(0, i);
+	s = aux;
 }
