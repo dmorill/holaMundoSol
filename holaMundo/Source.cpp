7a1486f0d8205984c13eb7c438cc9616fce8519c
Daniel Morillo Torres <dmorill@gmail.com>
2016-07-12 00:53:21 +0200

    Revert "Eliminar caracteres de txt"
    This reverts commit 9f3de751344c814ee2eab3f6180604576b76577d.

--- holaMundo/Source.cpp
+++ holaMundo/Source.cpp
@@ -1,40 +1,26 @@
 #include <iostream>;
-#include <fstream>;
-#include <string>;
-#include <experimental/filesystem>;
+#include <vector>;
 using namespace std;
 
-void path(string &s);
+void mostrarVector(vector<int> &v);
 
 int main() {
-	string donde;
-	path(donde);
-	donde = donde + "texto.txt";
-	fstream file;
+	vector<int> v;
 
-	file.open(donde, fstream::ate | fstream::in | fstream::out);	//ate es para empezar el cursor al fnial, es diferente de ::app
-	if (file.is_open()== false) {
-		cout << "Horror..."; 
-		return 0;
+	for (int i = 0; i < 10; i++) {
+		v.push_back(i);
 	}
-	long pos = file.tellp();
-	experimental::filesystem::resize_file(donde, pos-4);
-	file.close();
+	cout << "Hola mundo.";
 
-	//cin.get();
+	mostrarVector(v);
+
+	cin.get();
 	return 0;
 }
 
-void path(string &s) {
-	char* pchar;
-	_get_pgmptr(&pchar);
-	string aux(pchar); int y = 0, i = 0;
-	for (i = aux.size(); i >= 0; i--) {
-		if (aux[i - 1] == char(92)) {
-			y++;
-			if (y == 1) break;
-		}
+void mostrarVector(vector<int> &v) {
+	vector<int>::iterator it = v.begin();
+	for (; it != v.end(); it++) {
+		cout << "[" <<*it <<"]" << endl;
 	}
-	aux = aux.substr(0, i);
-	s = aux;
 }
