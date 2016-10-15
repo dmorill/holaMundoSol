f4f1352edd60d1830307efffed7be63a20066c78
Daniel Morillo Torres <dmorill@gmail.com>
2016-10-15 20:32:31 +0200

    leerAchirvos
    leer archivos

--- holaMundo/Source.cpp
+++ holaMundo/Source.cpp
@@ -1,34 +1,43 @@
 #include <iostream>;
 #include <vector>;
-
-#include <iostream>;
-#include <vector>;
-
+#include <string>;
+#include <fstream>
 
 using namespace std;
 
+string path(int x);
+
 int main() {
-	int i = 0;
-	vector<int> v(10, 50);
-	v.back() = 8;
+	string direDonde = path(1);		//obtener el path
 
-	int *pi = &(v[0]);
-	int *p = &v[9];
-	cout << "Dirección del puntero:  " << p << endl;
-	cout << "contenido del puntero:  " << *p << endl << endl;
+	fstream archivo(direDonde + "hola.txt", fstream::in);
+	if (archivo.is_open() == true) {
+		vector<string> vS;
+		string auxS;
+		while (archivo >> auxS) {
+			vS.push_back(auxS);
+		}
+		archivo.close();
+	}else {
+		cout << "horror!!!";
+	}
 	
-	v.push_back(100);
-	cout << "haciendo pusch_back" << endl << endl;
-	cout << "Dirección del puntero:  " << p << endl;
-	cout << "contenido del puntero:  " << *p << endl << endl;
-
-	int aux = p - pi;
-	p = &(v[0]) + aux;
-
-	cout << "aux = " << aux << endl;
-	cout << "Dirección del puntero:  " << p << endl;
-	cout << "contenido del puntero:  " << *p << endl << endl;
-
 	cin.get();
 	return 0;
+}
+
+
+string path(int x){
+	char* p;
+	_get_pgmptr(&p);
+	string direDonde(p);
+	int i = 0, iDonde = 0;
+	for (i = direDonde.size(); i >= 0; i--) {
+		if (direDonde[i - 1] == char(92)) {
+			iDonde++;
+			if (iDonde == x) break;
+		}
+	}
+	direDonde = direDonde.substr(0, i);
+	return direDonde;
 }
