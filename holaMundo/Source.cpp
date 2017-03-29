7eb8e177aefc36074cdb112ff68e7acb19ed3041
Daniel Morillo Torres <dmorill@gmail.com>
2017-03-29 16:30:33 +0200

    123

--- holaMundo/Source.cpp
+++ holaMundo/Source.cpp
@@ -1,25 +1,53 @@
-#include <iostream>;
-#include <vector>;
-#include <string>;
+#include <iostream>
+#include <vector>
+#include <string>
 #include <fstream>
 #include <ctime>
+#include <algorithm>
 using namespace std;
 
-string path(int x);
-
-int main(int argc, char** argv) {
-	// string direDonde = path(1);		//obtener el path
-	clock_t t1 = clock(), t2 = 0;
-	double total = 0;
-	for (int i = 0; i < 2000000000; i++) {
-		total += i;
+void mostar(vector<int>& v) {
+	vector<int>::iterator it = v.begin();
+	for (; it != v.end();it++) {
+		cout << *it << " ";
 	}
-	t2 = clock() - t1;
-	cout << t2 / double(CLOCKS_PER_SEC);
-	//cin.get();
-	return 0;
+	cout << endl;
 }
 
+//string path(int x);
+//void combinaRecu(vector<int>& v, int lim = 3);
+
+void permutate(vector<int>& s, vector<int>& index, int depth, int& count, vector<vector<int>>& vCombi) {
+	if (depth == s.size()) {
+		++count;
+		vCombi.push_back(vector<int>());
+		for (int i = 0; i < s.size(); ++i) {
+			vCombi.back().push_back(s[index[i]]);
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 0; i < s.size(); ++i) {
+		index[depth] = i;
+		permutate(s, index, depth + 1, count, vCombi);
+	}
+}
+
+int main() {
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	mostar(v);
+
+	vector<int> index(v.size());
+	int count = 0;
+	vector<vector<int>> vCombi;
+	permutate(v, index, 0, count, vCombi);
+	cout << "\nTotal permutations with repetitions: " << count;
+	return 0;
+}
 
 string path(int x){
 	char* p;
